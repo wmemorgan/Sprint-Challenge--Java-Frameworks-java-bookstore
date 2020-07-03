707d9e4d3bdc2e116270fa5919a21f81a4a4514f
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-03 22:11:28 +0800

    Add Section repository

---
+++ bookstore/src/main/java/com/lambdaschool/bookstore/repository/SectionRepository.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.bookstore.repository;
+
+import com.lambdaschool.bookstore.models.Section;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SectionRepository extends CrudRepository<Section, Long> {
+}
